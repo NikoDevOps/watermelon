f9ca6e6b2378aebff1aaab69f38cd3a3f1a97a8b
Nikolai <Nikolai@localhost>
2025-09-19 16:18:29 -0400

    version2: some updates

--- src/main/java/com/example/watermelon/WatermelonController.java
+++ src/main/java/com/example/watermelon/WatermelonController.java
@@ -9,13 +9,13 @@ import java.util.Map;
 @RestController
 public class WatermelonController {
 
-    @Value("${app.version:v1}")
+    @Value("${app.version:v2}")
     private String version;
 
     @GetMapping("/hello")
     public Map<String, String> hello() {
         return Map.of(
-                "message", "Hello from Watermelon",
+                "message", "Hello from Watermelon!!!",
                 "version", version
         );
     }
